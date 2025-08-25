57503ab48b58daad76d94946d1b324872236102a
Gustavo Stinghen <gustavo.s041@aluno.ifsc.edu.br>
2025-08-25 17:47:48 -0300

    CRUD Concluido
    Concluído o CRUD de Produtos (ler, editar, deletar, criar). Estilização realizada utilizando css pelo chat gpt.

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="url_index"),
     path('produtos', views.produtos, name="url_produtos"),
+    path('cadastrar-produto', views.cadastrarProduto, name="url_cadastrar_produto"),
+    path('atualizar-produto/<int:id>', views.atualizarProduto, name="url_editar_produto"),
+    path('deletar-produto/<int:id>', views.deletarProduto, name="url_deletar_produto"),
     path('404-preview/', views.custom_404, name='404_preview'),
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Produto
 
 # Aqui são feitas as views
@@ -14,6 +15,84 @@ def produtos ( request ):
         'produtos': produtos,
     }
     return render( request, 'produtos.html', context)
+
+def cadastrarProduto ( request ):
+
+    if request.method == "GET":
+        return render( request, 'novoProduto.html')
+    elif request.method == "POST":
+
+        nome = request.POST.get("nome")
+        
+        try:
+            preco = float(request.POST.get("preco").replace(',', '.'))
+        except:
+            preco = 0
+
+        try:
+            qtde = request.POST.get("qtde")
+        except:
+            qtde = 0
+
+        produto = Produto (
+            nome = nome,
+            preco = preco,
+            qtde = qtde
+        )
+
+        Produto.save(produto)
+
+        return redirect("url_produtos")
+
+def atualizarProduto (request, id ):
+
+    produto = get_object_or_404(Produto, id=id)
+
+    if request.method == "GET":
+        context = {
+            'produto': produto
+        }
+
+        return render(request, 'editarProduto.html', context)
+
+    elif request.method == "POST":
+
+        nome = request.POST.get("nome")
+        
+        try:
+            preco = float(request.POST.get("preco").replace(',', '.'))
+        except:
+            preco = 0
+
+        try:
+            qtde = request.POST.get("qtde")
+        except:
+            qtde = 0
+
+        produto.nome = nome
+        produto.qtde = qtde
+        produto.preco = preco
+        produto.save()
+        return redirect("url_produtos")
+
+def deletarProduto ( request, id ):
+
+    produto = get_object_or_404(Produto, id=id)
+
+    if request.method == "GET":
+
+        context = {
+            'produto': produto
+        }
+
+        return render(request, 'deletarProduto.html', context)
+
+    elif request.method == "POST":
+
+        produto.delete()
+
+
+    return redirect("url_produtos")
 
 def custom_404(request):
     return render(request, '404.html', status=404)
