0ee23ecaaf4b3f7c30ecb7f74682ff9a091d6a95
Gustavo Stinghen <gustavo.s041@aluno.ifsc.edu.br>
2025-09-01 17:10:02 -0300

    Fluxo de Login
    Realizado o fluxo de login e gerenciamento de usuário

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('cadastrar-produto', views.cadastrarProduto, name="url_cadastrar_produto"),
     path('atualizar-produto/<int:id>', views.atualizarProduto, name="url_editar_produto"),
     path('deletar-produto/<int:id>', views.deletarProduto, name="url_deletar_produto"),
+    path('entrar', views.entrar, name="url_entrar"),
+    path('cad_user', views.cad_user, name="url_cad_user"),
+    path('sair', views.sair, name="url_sair"),
     path('404-preview/', views.custom_404, name='404_preview'),
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Produto
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Aqui são feitas as views
 def index ( request ):
@@ -9,6 +13,7 @@ def index ( request ):
     }
     return render( request, 'index.html', context)
 
+@login_required(login_url="url_entrar")
 def produtos ( request ):
     produtos = Produto.objects.all()
     context = {
@@ -16,6 +21,7 @@ def produtos ( request ):
     }
     return render( request, 'produtos.html', context)
 
+@login_required(login_url="url_entrar")
 def cadastrarProduto ( request ):
 
     if request.method == "GET":
@@ -44,6 +50,7 @@ def cadastrarProduto ( request ):
 
         return redirect("url_produtos")
 
+@login_required(login_url="url_entrar")
 def atualizarProduto (request, id ):
 
     produto = get_object_or_404(Produto, id=id)
@@ -75,6 +82,7 @@ def atualizarProduto (request, id ):
         produto.save()
         return redirect("url_produtos")
 
+@login_required(login_url="url_entrar")
 def deletarProduto ( request, id ):
 
     produto = get_object_or_404(Produto, id=id)
@@ -93,6 +101,43 @@ def deletarProduto ( request, id ):
 
 
     return redirect("url_produtos")
+
+def entrar(request):
+    if request.method == "GET":
+        return render(request, "entrar.html")
+    else:
+        username = request.POST.get('nome')
+        password = request.POST.get('senha')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('url_produtos')
+        else:
+            return HttpResponse("Falha no login")
+
+def cad_user(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+
+        user = User.objects.filter(username=nome).first()
+
+        if user:
+            return HttpResponse("Usuário já existe")
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+
+        user.save()
+        messages.success(request, "Usuário cadastrado")
+        return render(request, "cad_user.html")
+    else:
+        return render(request, "cad_user.html")
+    
+def sair(request):
+    logout(request)
+    return redirect('url_entrar')
 
 def custom_404(request):
     return render(request, '404.html', status=404)
